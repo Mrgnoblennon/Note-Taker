0fa4f0747c840b74b9987ffe4f5f63a69594cb49
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-06-03 00:40:45 +0800

    added routes

--- develop/server.js
+++ develop/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3023;
 
 //middleware
-app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//routes
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+//serve static files from the "public" directory
+app.use(express.static('public'));
+
+//notes route
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-
-//add routes
-
+//default route 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port https://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
